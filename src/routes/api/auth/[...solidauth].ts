f7744e586718cf5a07f4d5553e5d5cc3f510521a
Mridang Agarwalla <mridang.agarwalla@gmail.com>
2025-08-11 22:08:52 +0530

    initial commit

--- src/routes/api/auth/[...solidauth].ts
+++ src/routes/api/auth/[...solidauth].ts
@@ -1,12 +1,15 @@
 import { authOptions } from '~/lib/auth';
 import { SolidAuth } from '@auth/solid-start';
+import { RequestEvent } from 'solid-js/web';
 
-export const GET = async (event: any) => {
+// noinspection JSUnusedGlobalSymbols
+export const GET = async (event: RequestEvent) => {
   const { GET } = SolidAuth(authOptions);
   return GET(event);
 };
 
-export const POST = async (event: any) => {
+// noinspection JSUnusedGlobalSymbols
+export const POST = async (event: RequestEvent) => {
   const { POST } = SolidAuth(authOptions);
   return POST(event);
 };
