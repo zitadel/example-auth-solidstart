fb645a677ebbc232fb69677046b99b8126f14ca2
Mridang Agarwalla <mridang.agarwalla@gmail.com>
2026-05-27 16:19:22 +0300

    chore: add X-Content-Type-Options + tsconfig moduleResolution PascalCase

--- knip.config.js
+++ knip.config.js
@@ -1,14 +1,12 @@
 module.exports = {
-  ignoreDependencies: [
-    '@commitlint/config-conventional'
-  ],
+  ignoreDependencies: ['@commitlint/config-conventional'],
   entry: [
     'src/entry-*',
     'src/app.tsx',
     'app.config.*',
     'src/routes/**/*',
     'src/components/**/*',
-    'src/lib/**/*'
+    'src/lib/**/*',
   ],
   ignore: ['commitlint.config.js', 'app.config.timestamp_*.js'],
 };
